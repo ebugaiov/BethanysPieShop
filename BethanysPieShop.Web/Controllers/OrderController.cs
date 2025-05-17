e56c2d8b2940e50a07debff199435ccc503c2608
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-05-17 19:26:19 +0200

    add Identity

--- BethanysPieShop.Web/Controllers/OrderController.cs
+++ BethanysPieShop.Web/Controllers/OrderController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using BethanysPieShop.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BethanysPieShop.Web.Controllers;
 
+[Authorize]
 public class OrderController : Controller
 {
     private readonly IOrderRepository _orderRepository;
--- BethanysPieShop.Web/Program.cs
+++ BethanysPieShop.Web/Program.cs
@@ -1,12 +1,19 @@
 using BethanysPieShop.Web.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 var dbUser = Environment.GetEnvironmentVariable("DB_USER");
 var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
 var connectionString = $"Server=localhost,1433;Database=BethanysPieShopDB;User Id={dbUser};Password={dbPassword};TrustServerCertificate=True;MultipleActiveResultSets=true;";
 
 var builder = WebApplication.CreateBuilder(args);
+// var connectionString = builder.Configuration.GetConnectionString("BethanysPieShopDbContextConnection") ?? throw new InvalidOperationException("Connection string 'BethanysPieShopDbContextConnection' not found.");
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<BethanysPieShopDbContext>();
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
@@ -20,12 +27,12 @@ builder.Services.AddControllersWithViews()
     .AddJsonOptions(opts => opts.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddRazorPages();
 
-builder.Services.AddDbContext<BethanysPieShopDbContext>(options => options.UseSqlServer(connectionString));
-
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
